c820b9a5c6eacf5048df0fd5a0be55e781ff3f3c
greenfructose <justinclarkturney@gmail.com>
2021-11-10 12:46:34 -0600

    Made front end WebUI functional

--- src/ui/apps.rs
+++ src/ui/apps.rs
@@ -9,3 +9,17 @@ pub async fn index(data: web::Data<AppData>, req: HttpRequest) -> impl Responder
     HttpResponse::Ok()
         .body(rendered)
 }
+
+pub async fn employees(data: web::Data<AppData>, req: HttpRequest) -> impl Responder {
+    let mut ctx = Context::new();
+    let rendered = data.tmpl.render("employees.html", &ctx).unwrap();
+    HttpResponse::Ok()
+        .body(rendered)
+}
+
+pub async fn departments(data: web::Data<AppData>, req: HttpRequest) -> impl Responder {
+    let mut ctx = Context::new();
+    let rendered = data.tmpl.render("departments.html", &ctx).unwrap();
+    HttpResponse::Ok()
+        .body(rendered)
+}
--- src/ui/server.rs
+++ src/ui/server.rs
@@ -1,7 +1,7 @@
 use actix_web::{HttpServer, web, App};
 use actix_files as fs;
 use tera::Tera;
-use crate::ui::apps::{index};
+use crate::ui::apps::{index, employees, departments};
 
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
@@ -15,6 +15,10 @@ pub async fn main() -> std::io::Result<()> {
             .service(fs::Files::new("/static", "./src/ui/static").show_files_listing())
             .service(web::resource("/")
                 .route(web::get().to(index)))
+            .service(web::resource("/employees")
+                .route(web::get().to(employees)))
+            .service(web::resource("/departments")
+                .route(web::get().to(departments)))
     })
     .bind("127.0.0.1:8080")?
     .run()
