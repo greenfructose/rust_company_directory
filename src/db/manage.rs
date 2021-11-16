6456e29c0ad9754a9ff604f7a1fc8ea91f0cda0f
greenfructose <justinclarkturney@gmail.com>
2021-11-16 16:46:05 -0600

    Working on the stupid ssl connection to the postgres db still

--- src/db/manage.rs
+++ src/db/manage.rs
@@ -1,25 +1,22 @@
-
-use native_tls::{Certificate, TlsConnector};
-use postgres_native_tls::MakeTlsConnector;
-use postgres::{Client, Error};
+use openssl::ssl::{SslConnector, SslMethod};
+use postgres_openssl::MakeTlsConnector;
+use postgres::Client;
 use crate::config::manage::get_db_config;
 use std::fs;
+use std::error::Error;
+
 
 // Get DB connection
-pub fn get_db_connection() -> Result<Client, Error> {
+pub fn get_db_connection() -> Result<Client, Box<dyn Error>> {
     let config = get_db_config();
     let cert_file = config.get("sslrootcert").unwrap().as_ref().unwrap();
-    let cert = fs::read(cert_file).unwrap();
-    let cert = Certificate::from_pem(&cert).unwrap();
-    let connector = TlsConnector::builder()
-        .add_root_certificate(cert)
-        .build().unwrap();
+    let mut builder = SslConnector::builder(SslMethod::tls())?;
+    builder.set_ca_file(cert_file)?;
+    let connector = MakeTlsConnector::new(builder.build());
     let connection_string = config.get("connection_string").unwrap().as_ref().unwrap();
-    let connector = MakeTlsConnector::new(connector);
-    println!("{}", connection_string);
     let client = Client::connect(
-        &*connection_string,
-        connector,
-    )?;
+        connection_string,
+        connector
+    ).unwrap();
     Ok(client)
 }
